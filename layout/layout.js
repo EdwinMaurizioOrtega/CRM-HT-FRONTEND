48b95a184f3d1440873115f6e2d81a03f8e0623c
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2023-01-13 11:30:47 -0500

    Ajustes 13Enero2022 11h30.

--- layout/layout.js
+++ layout/layout.js
@@ -106,7 +106,7 @@ const Layout = (props) => {
     return (
         <React.Fragment>
             <Head>
-                <title>Sakai by PrimeReact | Free Admin Template for NextJS</title>
+                <title>CRM | HT BUSINESS</title>
                 <meta charSet="UTF-8" />
                 <meta name="description" content="The ultimate collection of design-agnostic, flexible and accessible React UI Components." />
                 <meta name="robots" content="index, follow" />
@@ -118,6 +118,7 @@ const Layout = (props) => {
                 <meta property="og:image" content="https://www.primefaces.org/static/social/sakai-nextjs.png"></meta>
                 <meta property="og:ttl" content="604800"></meta>
                 <link rel="icon" href={`${contextPath}/favicon.ico`} type="image/x-icon"></link>
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
             </Head>
 
             <div className={containerClass}>
